d9b106271f741191a5d19eb8d23471981822a7cd
Slawomir Borowiec <przodownik@tlen.pl>
2015-12-08 09:32:07 +0100

    cleanup

--- src/main/java/pl/java/scalatech/aop/ServiceLoggingAspect.java
+++ src/main/java/pl/java/scalatech/aop/ServiceLoggingAspect.java
@@ -3,6 +3,7 @@ package pl.java.scalatech.aop;
 import javax.annotation.PostConstruct;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -20,13 +21,34 @@ public class ServiceLoggingAspect {
         log.info("###################################################### serviceloggingAspect");
     }
     
+   /* @Pointcut("@within(org.springframework.web.bind.annotation.RestController)")
+    public void controllerBean() {    
+    }
+    
+    @Pointcut("@within(org.springframework.web.bind.annotation.RequestMapping)")
+    public void requestMapping() {
+        log.info("################ Invoked: ");
+    }
+    
+    @Pointcut("execution(* *(..))")
+    public void method() {}*/
+    
+    
     @Pointcut("within(@org.springframework.web.bind.annotation.RestController *)")
-    public void controllerBean() {
+    public void withinController() {
+    }
 
+    @Pointcut("execution(* *(..))")
+    public void anyMethod() {
+    }
+
+    @AfterReturning("withinController() && anyMethod()")
+    public void whenControllerMethodReturns() {
+        log.info("++++++++++++++++++++++++++++++++++++++++++++++++ ");
     }
     
     
-    @Before("controllerBean()")
+    @Before("withinController() && anyMethod()")
     public void beforeWithin(JoinPoint joinPoint){
         log.info("++++++++++++++++++++++++++++++++++++++++++++++++ {}",joinPoint.getSignature().getName());
         
